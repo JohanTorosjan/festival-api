e768d307457bc2d75ccc10714a76309e26a9b2f2
Nuihau <nuihau.teena@etu.umontpellier.fr>
2023-02-14 20:56:06 +0100

    merged with main

--- src/jeux/jeux.controller.ts
+++ src/jeux/jeux.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
-import { addZoneDto } from './addzone.jeux.dto';
+import { identity } from 'rxjs';
 import { CreateJeuxDto } from './create.jeux.dto';
 import { JeuxService } from './jeux.service';
 
@@ -32,8 +32,4 @@ export class JeuxController {
         return this.jeuxService.getById(id)
     }
 
-    @Post('/zone/:id')
-    addToZone(@Param('id')id:string,@Body()addZoneDto:addZoneDto){
-       return this.jeuxService.addToZone(id,addZoneDto)
-    }
 }
